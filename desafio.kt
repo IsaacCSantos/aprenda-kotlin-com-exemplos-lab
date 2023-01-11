27b54cc28e1030c431546b10163a5bfbf858082f
Isaac Correia dos Santos <55966207+IsaacCSantos@users.noreply.github.com>
2023-01-10 21:31:26 -0300

    Entrega do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,52 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val matricula:String, var nome:String, var email:String)
 
-class Usuario
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    fun listarInscritos(){
+        inscritos.forEach{println("${it.matricula} - ${it.nome} - ${it.email}")}
+    }
+    
+    fun listarConteudos(){
+        for (i in conteudos){
+            println("${i.nome} - Duração ${i.duracao} minutos - Nivel: ${i.nivel}")
+        } 
+    }
+    fun buscarInscrito(matricula:String){
+    	inscritos.filter{it.matricula == matricula}.forEach{println("${it.matricula} - ${it.nome} - ${it.email}")}
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuarioIsaac = Usuario("1","Isaac","isaac@teste.com.br")
+    val usuarioJoao = Usuario("2","João", "joao@teste.com.br")
+  	val listaConteudo = mutableListOf<ConteudoEducacional>(
+    	ConteudoEducacional("Conhecendo o Kotlin e sua Documentação Oficial", 60, Nivel.BASICO),
+        ConteudoEducacional("Introdução Prática à Linguagem de Programação Kotlin", 120, Nivel.BASICO),
+        ConteudoEducacional("Estrutura de Controle de Fluxo e Coleções em Kotlin", 120, Nivel.BASICO),
+        ConteudoEducacional("Orientação a Objetos e Tipos de Classes na Prática com Kotlin", 120, Nivel.INTERMEDIARIO),
+        ConteudoEducacional("O poder das Funções Kotlin", 60, Nivel.AVANCADO)
+    );
+    
+    val formacao = Formacao("Kotlin Experience", listaConteudo, Nivel.BASICO)
+    formacao.matricular(usuarioIsaac)
+    formacao.matricular(usuarioJoao)
+    println("-----Dados da Formacao------")
+    println("Formacao: ${formacao.nome}")
+    println("Nivel: ${formacao.nivel}")
+    println("----------Inscritos---------")
+	formacao.listarInscritos()
+    println("----------Conteudo----------")
+    formacao.listarConteudos()
+    println("----------------------------")
+    formacao.buscarInscrito("1")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
